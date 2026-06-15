a5d8439a2693729b8609ccdffd6297439eea4222
theluismen <luismaocampo28@gmail.com>
2026-06-15 13:20:39 +0200

    feat: kernel stack impl

---
+++ Makefile
@@ -0,0 +1,25 @@
+OBJECTS = loader.o kernel.o
+CC = gcc
+CFLAGS = -O0 -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -ffreestanding -Wall -Wextra -Werror -c
+LDFLAGS = -T link.ld -melf_i386
+AS = nasm
+ASFLAGS = -f elf32
+
+
+all: kernel.elf
+	grub-mkrescue -o niggel.iso iso -d /usr/lib/grub/i386-pc
+
+kernel.elf: $(OBJECTS)
+	ld $(LDFLAGS) $(OBJECTS) -o iso/boot/kernel.elf
+
+%.o: src/%.c
+	$(CC) $(CFLAGS) $< -o $@
+
+%.o: src/%.s
+	$(AS) $(ASFLAGS) $< -o $@
+
+run:
+	bochs -f bochsrc
+
+clean:
+	rm -rf *.o iso/boot/kernel.elf niggel.iso
--- src/loader.s
+++ src/loader.s
@@ -1,8 +1,11 @@
 BITS 32
 
-MAGIC_NUMBER    equ 0x1BADB002
-FLAGS           equ 0x0
-CHECKSUM        equ -(MAGIC_NUMBER + FLAGS)
+MAGIC_NUMBER        equ 0x1BADB002
+FLAGS               equ 0x0
+CHECKSUM            equ -(MAGIC_NUMBER + FLAGS)
+KERNEL_STACK_SIZE   equ 4096
+
+extern test
 
 section .multiboot
 align 4
@@ -14,6 +17,17 @@ section .text
 
 global _start
 _start:
-    mov     eax, 0xABCDEF00
+    mov     esp, kernel_stack + KERNEL_STACK_SIZE
+    push    dword 6
+    push    dword 7
+    call    test
+    add     esp, 8
 .end:
     jmp     .end
+
+section .bss
+align 4
+kernel_stack:
+    resb    KERNEL_STACK_SIZE
+
+section .note.GNU-stack noalloc
